ad7bb71c570c17dc03b54d9d8491006a2304505b
RebecaRSS-Dev <rebecasoares.tech@gmail.com>
2025-07-31 09:40:31 -0300

    testando movimentos

--- movimentoByte.py
+++ movimentoByte.py
@@ -15,17 +15,18 @@ byte_height = 60
 byte_x = 50
 byte_y_inicial = ALTURA - byte_height - 10
 byte_y = byte_y_inicial
-byte_jump_speed = - 15
-byte_gravity = 0.8
+byte_jump_speed_inicial = -15
+byte_jump_speed = byte_jump_speed_inicial
+byte_gravity = 1
 
-largura_do_obstaculo = 30
-altura_do_obstaculo = 70
+obstaculo_width = 30
+obstaculo_height = 70
 obstaculo_x = LARGURA 
-obstaculo_y_inicial = ALTURA - altura_do_obstaculo - 10
+obstaculo_y_inicial = ALTURA - obstaculo_height - 10
 obstaculo_y = obstaculo_y_inicial
 velocidade_do_obstaculo = 5
 
-relógio = pygame.time.Clock() 
+clock = pygame.time.Clock() 
 
 executando = True
 
@@ -34,4 +35,38 @@ while(executando):
         if  evento.type == pygame.QUIT: 
             running = False
         elif evento.type == pygame.KEYDOWN: 
-            if  evento.key == pygame.K_SPACE and byte_y == byte_y_inicial : byte_jump_speed             = - 15
+            # if  evento.key == pygame.K_SPACE and byte_y == byte_y_inicial: 
+            #     byte_jump_speed = byte_jump_speed_inicial
+            if evento.key == pygame.K_UP:
+                byte_jump_speed = byte_y_inicial - 50
+        byte_jump_speed += byte_gravity
+        
+    byte_y += byte_jump_speed 
+
+    if byte_y > byte_y_inicial: 
+        byte_y = byte_y_inicial
+        byte_jump_speed = 0
+        
+    # obstaculo_x -= velocidade_do_obstaculo
+    
+    # if obstaculo_x < -obstaculo_width: 
+    #     obstaculo_x = LARGURA 
+    #     obstaculo_y = obstaculo_y_inicial 
+
+    if ( 
+        byte_x + byte_width > obstaculo_x 
+        and byte_x < obstaculo_x + obstaculo_width 
+        and byte_y + byte_height > obstaculo_y 
+    ):
+        executando = False 
+
+    tela.fill(BRANCO) 
+
+    pygame.draw.rect(tela, PRETO, (byte_x, byte_y, byte_width, byte_height))
+    pygame.draw.rect(tela, PRETO, (obstaculo_x, obstaculo_y, obstaculo_width, obstaculo_height)) 
+
+    pygame.display.flip() 
+
+    clock.tick( 60 )
+
+pygame.quit() 
